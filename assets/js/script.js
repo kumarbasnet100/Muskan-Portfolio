9437f64dcbc769fbe0e6ef747142035e59c0b5b2
Kumar Basnet <kumarkingxeetri100@gmail.com>
2025-04-11 13:46:36 +0545

    fixes bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,11 +71,11 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Muskan Rijal";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/hero.png");
         }
         else {
             document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            $("#favicon").attr("href", "assets/images/hero.png");
         }
     });
 
